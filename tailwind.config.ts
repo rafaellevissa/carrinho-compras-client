2e110aaf888e85f7aa89f79fe5dd1baaf19621ec
rafaellevissa <rafaellevissa@gmail.com>
2024-07-02 20:52:21 -0300

    Set fontfamily

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -4,10 +4,12 @@ const config: Config = {
   content: [
     "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
     extend: {
+      fontFamily: {
+        beiruti: ["Beiruti", "sans-serif"],
+      },
       backgroundImage: {
         "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
         "gradient-conic":
