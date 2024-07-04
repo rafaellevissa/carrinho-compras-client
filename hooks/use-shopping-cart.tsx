e0adb0b90408b02e1f143ebea407fd7b7d868996
rafaellevissa <rafaellevissa@gmail.com>
2024-07-04 13:34:06 -0300

    Remove hooks and contexts

--- hooks/use-shopping-cart.tsx
+++
@@ -1,6 +0,0 @@
-import { ShoppingCartContext } from "@/contexts/shopping-cart";
-import { useContext } from "react";
-
-const useShoppingCartContext = () => useContext(ShoppingCartContext);
-
-export default useShoppingCartContext;
