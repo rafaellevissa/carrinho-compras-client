24419fd946df72c2f2a98b97b09f63ad4c061777
rafaellevissa <rafaellevissa@gmail.com>
2024-07-03 21:45:41 -0300

    Change structure to nextjs app route

--- src/components/Footer.tsx
+++
@@ -1,9 +0,0 @@
-const Footer = () => {
-  return (
-    <footer className="mt-5 text-center py-4 border-t border-neutral-600">
-      <p>© {new Date().getFullYear()} Allu. All rights reserved.</p>
-    </footer>
-  );
-};
-
-export default Footer;
--- src/pages/_app.tsx
+++
@@ -1,7 +0,0 @@
-import "../styles/globals.css";
-
-function HomePage({ Component, pageProps }: any) {
-  return <Component {...pageProps} />;
-}
-
-export default HomePage;
